59e9d93f3de3c12c90e71899a173d42f14b522f7
seunghee <kwakshma@mokpo.ac.kr>
2023-07-19 03:42:18 -0400

    second commit

--- main.cpp
+++ main.cpp
@@ -8,15 +8,15 @@ int main(int argc, char *argv[]) {
 	uint32_t val1, val2;
 	
 	fp1 = fopen(argv[1], "rb");
-    fp2 = fopen(argv[2], "rb");
+	fp2 = fopen(argv[2], "rb");
 
-    fread(&val1, sizeof(val1), 1, fp1);
-    fread(&val2, sizeof(val2), 1, fp2);
+	fread(&val1, sizeof(val1), 1, fp1);
+	fread(&val2, sizeof(val2), 1, fp2);
 	
 	add(val1, val2);
 	
 	fclose(fp1);
-    fclose(fp2);
+	fclose(fp2);
 
-    return 0;
+	return 0;
 }
